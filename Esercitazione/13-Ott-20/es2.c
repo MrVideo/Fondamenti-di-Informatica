88d3825c0b97e0cf16706a8da3a67d15a9dc6302
Mario <MrVideo@users.noreply.github.com>
2020-10-13 11:05:42 +0200

    Added es2 source and binary

---
+++ Esercitazione/13-Ott-20/es2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Corretti gli errori
+
+int main()
+{
+    int y = 0;
+    int x;
+    x = 1;
+    printf("La variabile x contiene: %d\n", x);
+    printf("La variabile y contiene: %d\n", y);
+    return 0;
+}
