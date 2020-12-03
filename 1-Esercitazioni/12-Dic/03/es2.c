c840b12ec1b8ce091dcd08dd03a58c9f1985a179
Mario Merlo <m20_11@icloud.com>
2020-12-03 15:52:11 +0100

    Added es2

---
+++ 1-Esercitazioni/12-Dic/03/es2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct nodo
+{
+    int data;
+    struct nodo *next;
+};
+
+typedef struct nodo *lista;
+
+lista selezione(lista l, int sel[]);
+lista inserisci_in_testa(lista l, int elem);
+lista inserisci_in_coda(lista l, int elem);
+
+void stampa(lista l);
+
+int main()
+{
+    lista l = NULL;
+
+    l = inserisci_in_coda(l, 20);
+    l = inserisci_in_coda(l, 16);
+    l = inserisci_in_coda(l, 12);
+    l = inserisci_in_coda(l, 2);
+
+    stampa(l);
+
+    int arr[4] = {0, 1, 1, 0};
+
+    lista l2 = selezione(l, arr);
+
+    stampa l;
+}
+
+lista selezione(lista l, int sel[])
+{
+    if(l == NULL)
+        return l;
+    else if(sel[0] == 0)
+        return selezione(l -> next, sel + 1);
+    else
+    {
+        struct nodo *p;
+        p = malloc(sizeof(struct(nodo)));
+
+        p -> elem = l -> elem;
+        p -> next = selezione(l -> next, sel + 1);
+        
+        return p;
+    }
+}
+
+lista inserisci_in_testa(lista l, int elem)
+{
+
+}
+
+lista inserisci_in_coda(lista l, int elem)
+{
+
+}
