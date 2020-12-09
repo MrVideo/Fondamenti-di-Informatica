2612e12a031dae269eee68820582d85c225581d0
Mario Merlo <m20_11@icloud.com>
2020-12-09 16:03:30 +0100

    Added es3.c and its binary

--- 1-Esercitazioni/12-Dic/09/es3.c
+++ 1-Esercitazioni/12-Dic/09/es3.c
@@ -2,8 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+int print_div(char *num);
+
 int main()
 {
+    char num[10];
+    
+    system("clear");
 
+    printf("Inserisci un numero: ");
+    scanf("%s", num);
+
+    printf("\nIl numero di divisori contenuti nel numero è %d\n", print_div(num));
+    
+    return 0;
 }
 
+int print_div(char *num)
+{
+    int number = atoi(num), i, digit, divCounter = 0;
+
+    for(i = 0; i < strlen(num); i++)
+    {
+        digit = (int)num[i] - 48;        
+
+        if(number % digit == 0)
+            divCounter++;
+    }
+
+    return divCounter;
+}
