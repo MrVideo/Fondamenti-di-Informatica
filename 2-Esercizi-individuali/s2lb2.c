0042f4cf8f67aa6d5fbb798ed144f378b447db79
Mario Merlo <m20_11@icloud.com>
2020-12-30 20:58:18 +0100

    New exercise

--- 2-Esercizi-individuali/s2lb2.c
+++ 2-Esercizi-individuali/s2lb2.c
@@ -3,98 +3,73 @@
 #include <string.h>
 
 struct node {
-    char c;
+    char letter;
     int freq;
     struct node *next;
 };
 
 typedef struct node *list;
 
-list string2list(char *string);
-list search(list l, char c);
-list add(list l, char c, int f);
-list append(list l, char c, int f);
+list string2list(char *s);
+list add(list l, char c);
 void print(list l);
 
 int main()
 {
-    char string[30];
-    list l;
+    list l = NULL;
+    int i;
 
     system("clear");
 
-    printf("Inserisci un messaggio: ");
-    scanf("%s", string);
-
-    l = string2list(string);
+    l = string2list("esempio");
 
     print(l);
 
     return 0;
 }
 
-list string2list(char *string)
+list string2list(char *s)
 {
-    int i, j, count = 0;
-    list l = malloc(sizeof(struct node)), current = malloc(sizeof(struct node)); //Creazione lista
+    list l;
+    int i;
+    
+    for(i = 0; i < strlen(s); i++)
+        l = add(l, *(s + i));
 
-    for(i = 0; i < strlen(string); i++) //Scorre la stringa
+    return l;
+}
+
+list add(list l, char c)
+{
+    struct node *current, *tmp;
+
+    current = l;
+
+    while(current != NULL && current -> letter != c)
+        current = current -> next;
+
+    if(current != NULL)
+        current -> freq = current -> freq + 1;
+    else
     {
-        count = 0;
-        current = search(l, *(string + i)); //Cerca elementi già aggiunti
-        printf("\nSearch OK\n");
+        tmp = malloc(sizeof(struct node));
+        tmp -> letter = c;
+        tmp -> freq = 1;
+        tmp -> next = l;
 
-        if(current == NULL) //Se non c'è questo elemento
-        {
-            for(j = 0; j < strlen(string); j++) //Conta le occorrenze di un carattere
-            {
-                if(*(string + j) == *(string + i))
-                    count++;
-            }
-
-            l = append(l, *(string + i), count); //Aggiungi alla lista
-        }
+        l = tmp;
     }
 
     return l;
 }
 
-list search(list l, char c) //Ricerca
+void print(list l)
 {
-    if(l -> c == c || l == NULL) //Se la lista è vuota o se l'elemento cercato è nel nodo corrente
-        return l; //Ritorna quell'indirizzo
-    else //Altrimenti
-        return search(l -> next, c); //Cerca nel prossimo nodo
-}
-
-list add(list l, char c, int f) //Aggiungi in testa
-{
-    list tmp = malloc(sizeof(struct node)); //Crea un nodo temporaneo
-    tmp -> c = c; //Poni nel nuovo nodo il carattere in ingresso...
-    tmp -> freq = f; //... la frequenza di quel carattere...
-    tmp -> next = l; //... e l'indirizzo della vecchia testa
-
-    return tmp; //Ritorna il nuovo nodo
-}
-
-list append(list l, char c, int f) //Aggiungi in coda
-{
-    if(l == NULL) //Se la lista è vuota
-        return add(l, c, f); //Aggiungi in testa
-    else //Altrimenti
+    if(l == NULL)
+        printf("END\n");
+    else
     {
-        l -> next = append(l -> next, c, f); //Passa al nodo successivo e controlla se è l'ultimo
-        return l; //Ritorna l'indirizzo di quel nodo
-    }
-}
-
-void print(list l) //Stampa la lista
-{
-    if(l == NULL) //Se la lista è alla fine o è vuota
-        printf("END\n"); //Stampa END
-    else //Altrimenti
-    {
-        printf("%c / %d -> ", l -> c, l -> freq); //Stampa carattere e frequenza di un nodo
-        print(l -> next); //E passa al prossimo
+        printf("[ %c | %d ] -> ", l -> letter, l -> freq);
+        return print(l -> next);
     }
 }
